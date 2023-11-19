cb28b9a77e011b37bac11144df133f450e8a4b86
Данила Андреев <Danilka85@outlook.com>
2023-11-19 21:12:55 +0300

    Добавьте файлы проекта.

---
+++ PP.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream> 
+using namespace std;
+int main()
+{
+	int x, y;
+	cout << "Enter an x\n"; cin >> x;
+	cout << "Enter an y\n"; cin >> y;
+	cout << x << "+" << y << "=" << x + y << endl;
+	cout<< x << "-" << y << "=" << x - y;
+}
+
+
