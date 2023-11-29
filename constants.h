0404619d89782000df2b677488235f74e9bc1139
KotoBaka <danilka85@outlook.com>
2023-11-29 14:59:32 +0300

    задание №3 (на основе задания №2)

--- constants.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-constexpr double g{9.8};	// в метрах/секунда в квадрате
-
-#endif
--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
-﻿#include <iostream>
-#include "myFunc.h"
-#include "constants.h"
+﻿#include <bitset>
+#include <iostream>
+
+// "rotl" означает "вращать влево"
+std::bitset<4> rotl(std::bitset<4> bits)
+{
+	std::bitset<4> mask {0b1000};
+	std::bitset<4> buf{bits};
+	buf |= mask;
+	if (buf != bits)
+		return (bits << 1);
+	else
+		bits <<= 1;
+		bits.flip(0);
+		return (bits);
+}
 
 int main()
 {
-	std::cout << "Enter the height of the tower in meters: ";
-	double h{};		// Высота (метры)
-	std::cin >> h;
-	uint_fast16_t t{};		// Время в падении (секунды)
+	std::bitset<4> bits1{ 0b0001 };
+	std::cout << rotl(bits1) << '\n';
 
-	for (; t <= 5; t++)
-	{
-		if (calculateH(h, t) > 0)
-			std::cout << "At " << t << " seconds, the ball is at height : " << calculateH(h, t) << " meters\n";
-		else 
-			std::cout << "At " << t << " seconds, the ball is on the ground\n";
-	}
+	std::bitset<4> bits2{ 0b1001 };
+	std::cout << rotl(bits2) << '\n';
 
 	return 0;
 }
