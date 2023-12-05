f2946a27145dedc5951a77eb014ad6d1def4fce1
KotoBaka <danilka85@outlook.com>
2023-12-05 19:00:52 +0300

    вопрос №6

--- main.cpp
+++ main.cpp
@@ -1,27 +1,40 @@
-﻿#include <bitset>
-#include <iostream>
+﻿#include <iostream>
 
-// "rotl" означает "вращать влево"
-std::bitset<4> rotl(std::bitset<4> bits)
+void bitout(int numb)
 {
-	std::bitset<4> mask {0b1000};
-	std::bitset<4> buf{bits};
-	buf |= mask;
-	if (buf != bits)
-		return (bits << 1);
-	else
-		bits <<= 1;
-		bits.flip(0);
-		return (bits);
+
+	int c{ 128 };
+	do {
+
+		if (c == 8) std::cout << ' ';
+		if (numb >= c)
+		{
+			numb -= c;
+			c /= 2;
+			std::cout << 1;
+		}
+		else
+		{
+			c /= 2;
+			std::cout << 0;
+		}
+
+	} while (c > 0);
+
 }
 
 int main()
 {
-	std::bitset<4> bits1{ 0b0001 };
-	std::cout << rotl(bits1) << '\n';
+	int numb;
+	std::cout << "enter a integer number from 0 to 255\n" << "number: ";
+	std::cin >> numb;
 
-	std::bitset<4> bits2{ 0b1001 };
-	std::cout << rotl(bits2) << '\n';
-
+	if (numb > 255)
+		std::cout << "number greater than 255";
+	else
+	{
+		std::cout << numb << " in 8-bit form: ";
+		bitout(numb);
+	}
 	return 0;
 }
